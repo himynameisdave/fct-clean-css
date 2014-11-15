e01316c1ab9826555d9310af5cacc6a65eb15f77
Dave Lunny <sir.dave@me.com>
2014-11-15 13:14:14 -0500

    added tab support and general cleanup/commenting
    NOW its ready for a merge.

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,9 @@
 //	Written by Dave Lunny
 
+//////////////////////////////////////
+//	 		STRING PROTOS			//
+//////////////////////////////////////
+
 //	We need all strings to be able to check if they are empty or not...
 String.prototype.isEmptyString = function() {
 
@@ -20,10 +24,85 @@ String.prototype.insert = function (index, string) {
 //	All strings can now remove a 'line'
 String.prototype.deleteLine = function ( index ) {
 
-
 };
 
+//////////////////////////////////////
+//	   TEXTAREA FOCUS/TAB PROTO 	//
+//////////////////////////////////////
 
+//	Thanks CSS-Tricks!
+//	http://css-tricks.com/snippets/javascript/support-tabs-in-textareas/
+HTMLTextAreaElement.prototype.getCaretPosition = function () { //return the caret position of the textarea
+    return this.selectionStart;
+};
+HTMLTextAreaElement.prototype.setCaretPosition = function (position) { //change the caret position of the textarea
+    this.selectionStart = position;
+    this.selectionEnd = position;
+    this.focus();
+};
+HTMLTextAreaElement.prototype.hasSelection = function () { //if the textarea has selection then return true
+    if (this.selectionStart == this.selectionEnd) {
+        return false;
+    } else {
+        return true;
+    }
+};
+HTMLTextAreaElement.prototype.getSelectedText = function () { //return the selection text
+    return this.value.substring(this.selectionStart, this.selectionEnd);
+};
+HTMLTextAreaElement.prototype.setSelection = function (start, end) { //change the selection area of the textarea
+    this.selectionStart = start;
+    this.selectionEnd = end;
+    this.focus();
+};
+
+$(document).ready(function(){
+
+	var textarea = document.getElementsByTagName('textarea')[0]; 
+
+	textarea.onkeydown = function(event) {
+	    
+	    //support tab on textarea
+	    if (event.keyCode == 9) { //tab was pressed
+	        var newCaretPosition;
+	        newCaretPosition = textarea.getCaretPosition() + "    ".length;
+	        textarea.value = textarea.value.substring(0, textarea.getCaretPosition()) + "    " + textarea.value.substring(textarea.getCaretPosition(), textarea.value.length);
+	        textarea.setCaretPosition(newCaretPosition);
+	        return false;
+	    }
+	    if(event.keyCode == 8){ //backspace
+	        if (textarea.value.substring(textarea.getCaretPosition() - 4, textarea.getCaretPosition()) == "    ") { //it's a tab space
+	            var newCaretPosition;
+	            newCaretPosition = textarea.getCaretPosition() - 3;
+	            textarea.value = textarea.value.substring(0, textarea.getCaretPosition() - 3) + textarea.value.substring(textarea.getCaretPosition(), textarea.value.length);
+	            textarea.setCaretPosition(newCaretPosition);
+	        }
+	    }
+	    if(event.keyCode == 37){ //left arrow
+	        var newCaretPosition;
+	        if (textarea.value.substring(textarea.getCaretPosition() - 4, textarea.getCaretPosition()) == "    ") { //it's a tab space
+	            newCaretPosition = textarea.getCaretPosition() - 3;
+	            textarea.setCaretPosition(newCaretPosition);
+	        }    
+	    }
+	    if(event.keyCode == 39){ //right arrow
+	        var newCaretPosition;
+	        if (textarea.value.substring(textarea.getCaretPosition() + 4, textarea.getCaretPosition()) == "    ") { //it's a tab space
+	            newCaretPosition = textarea.getCaretPosition() + 3;
+	            textarea.setCaretPosition(newCaretPosition);
+	        }
+	    } 
+	};
+});
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+//									APP 										//
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 var app = angular.module('app', [] );
 
 app.controller('Controller', function ($scope, $log) {
@@ -91,37 +170,47 @@ app.controller('Controller', function ($scope, $log) {
 	};
 
 	var convertToRGB = function( str ){
-		//	the string we can fuck with
+		//	the string we can fuck with (and have it be different from the OG)
 		var tempStr = str;
 
-		//	STEP 1: Find instances of '#000000' OR '#000'
+		//	anything that starts with '#' and ends with ';' and is between 3 - 6 characters
+		//	TODO: in case the user does something dumb like "color: #000 ;"
 		var hexes = tempStr.match(/#.{3,6};/img);
 
-		//	STEP 2: check if there are any hexes in the CSS
+		//	if there aren't any hexes found, skip this and return the unfucked with tempStr
 		if( hexes != null ){
+			//	since there are hex codes found, lets loop through em.
 			$.each(hexes, function( i, val ){
 
-				//	BUG: this doesn't handle shortcodes very well at all - convert them up to 6 digits
+				//	get rid of the '#' and ';'
+				var  realVal  = val.replace("#","").replace(";",""),
+					 hexLength = realVal.length;
 
-				var  realVal  = val.replace("#","").replace(";","");
+				//	if it's a short hexcode, convert it to a 'real' 6 digit one
+				if( hexLength === 3 ) {
 
-				if( realVal.length === 3 ) {
+					//	get each number
+					var r = realVal.slice(0,1),
+					    g = realVal.slice(1,2),
+					    b = realVal.slice(2,3),
+					    //	times two!
+					    realVal = r+r+g+g+b+b;
+				};
 
-					var r = realVal.slice(0,1) + realVal.slice(0,1),
-					    g = realVal.slice(1,2) + realVal.slice(1,2),
-					    b = realVal.slice(2,3) + realVal.slice(2,3),
-					    realVal = r+g+b;
-
+				//	if it's some f-worded up value, just leave it (coders mistake);
+				if( hexLength < 3 || ( hexLength < 6 && hexLength != 3 ) || hexLength > 6 ){
+					console.log('Someone doenst know what a hex val is!');
+				}else{
+					var	 rgbVals  = toRGB(realVal),// thanks to Dan Lamb for his hex-to-rgb microlibrary @ https://github.com/daniellmb/HEX-RGB-Conversion
+						 rgbStr   = 'rgb( '+rgbVals[0]+', '+rgbVals[1]+', '+rgbVals[2]+' );';
+					// rgbs.push(rgbStr);
+					tempStr = tempStr.replace( val, rgbStr );
 				}
 
-				var	 rgbVals  = toRGB(realVal),// thanks to Dan Lamb for his hex-to-rgb microlibrary @ https://github.com/daniellmb/HEX-RGB-Conversion
-					 rgbStr   = 'rgb( '+rgbVals[0]+', '+rgbVals[1]+', '+rgbVals[2]+' );';
-				// rgbs.push(rgbStr);
-				tempStr = tempStr.replace( val, rgbStr );
-			});
-		};
+			});// end loop through each hex
+		};// end check if there are any hexes
 
-		//	always return that altered string
+		//	always return that (potentially) altered string
 		return tempStr;
 	};
 
