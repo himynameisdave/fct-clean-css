02237a6750993f2cc2eef39d0b6314dba1c0829a
Dave Lunny <sir.dave@me.com>
2014-11-15 11:26:12 -0500

    lol removed routes (leftover from copying routes)
    ye ye ye

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ fct-clean-css
 =============
 
 Formats CSS to FCT Standards
+
+
+Building as a web app first to perfect the script, then going to build it as a node/grunt module.
--- app/js/app.js
+++ app/js/app.js
@@ -61,69 +61,6 @@ app.controller('Controller', function ($scope, $log) {
 		///CALLING THE RGB/HEX CONVERSION
 		newCleanCSS = convertToRGB( newCleanCSS );
 
-/////////////////
-/////////TO BE REFACTORED
-/*
-
-		//	loops through each set of properties
-		$.each( selectorItems, function( i, val ){
-			var replacementText = val;
-
-			//	This will break each line of the block into it's own string, stored in an array
-			var lines = replacementText.split('\n');
-
-			// $log.log('lines length: '+lines.length);
-
-			var noEmptyLines = '';
-			//	Cycle through the lines and remove any empty ones (except the first and last);
-			$.each( lines, function( j, lineVal ){
-
-				//	ignore first and last
-				if( j > 0 && j < lines.length ){
-					//	if the string is empty, get rid of it
-					if( lineVal.isEmptyString() ){
-
-
-
-						lines.splice(j, 1);
-					
-					}else{
-						noEmptyLines += lines[j];
-					} 
-				}
-			});
-
-			// $log.log('lines length (After): ' + lines.length);
-
-			$log.log('replacementText becomes: ');
-			$log.log(noEmptyLines);
-
-			replacementText = noEmptyLines;
-			// replacementText = lines.join('\n');
-			// console.log( replacementText );
-
-			//	Instead of even seeing if its the last one, we just grab the last one
-			//	TODO: if its not the last one, remove the first one - this will probably be a global function eventually to remove dupes
-			var marginIndex = replacementText.lastIndexOf("margin:"),
-				paddingIndex = replacementText.lastIndexOf("padding:");
-
-			//	Checks for any shorthand use of 'margin:' or 'padding:' and returns the block with shorthands expanded
-			replacementText = shorthandHandler(replacementText, marginIndex, paddingIndex);
-
-
-			// $log.log(replacementText.indexOf(";"));
-
-			
-
-			//	This next pair of ifs go check to see if shorthand margins and paddings 
-			
-
-			// 	ends with a replace of the old stuff with the new stuff
-			newCleanCSS.replace( val, replacementText );
-		});
-*/
-
-
 		//	Reset the clean side of the CSS with the new clean CSS
 		$scope.cleanCSS = '';
 		$scope.cleanCSS = newCleanCSS;
@@ -155,7 +92,51 @@ app.controller('Controller', function ($scope, $log) {
 	};
 
 	var convertToRGB = function( str ){
+		//	the string we can fuck with
+		var tempStr = str;
 
+		//	STEP 1: Find instances of '#000000' OR '#000'
+		var hexes = tempStr.match(/#.{3,6};/img);
+
+		//	STEP 2: Create a mirror array of rgb() values
+		var rgbs = [];
+
+		//	check if there are any hexes in the CSS
+		if( hexes != null ){
+			$.each(hexes, function( i, val ){
+				//without all the '#' and ';'
+				var realLength = val.length - 2;
+
+				//	STEP 2A): See if any of the hexes are mini hexes
+				$log.log(realLength);
+				//	Expand mini hexes
+				if( realLength === 3 ){
+
+				};
+				//	Could catch invalid hexes here:
+				if( realLength > 3 && realLength < 6 ){
+
+				};
+
+
+
+			});
+		};
+
+
+		// R = hexToR("#FFFFFF");
+		// G = hexToG("#FFFFFF");
+		// B = hexToB("#FFFFFF");
+
+		// function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
+		// function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
+		// function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
+		// function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
+
+
+
+		//	always return that altered string
+		return tempStr;
 	};
 
 	//	returns the number of times a substring appears in a string
@@ -163,78 +144,6 @@ app.controller('Controller', function ($scope, $log) {
 		var r = new RegExp( val, 'g');
 		return (str.match(r) || []).length;
 	};
-
-/////////////////
-/////////TO BE REFACTORED
-/*
-
-	//	Handler for when shorthand 'margin' or 'padding' are used
-	var shorthandHandler = function ( str, m, p ) {
-
-		//	If there is an instance of 'margin:'
-		if ( m >= 0 ){	
-
-			var trash, copyStr = str;
-			//	The plus 7 because there are 7 characters in 'margin:'
-			
-			//	get rid of everything before 'margin:' (everything up until last line break)
-			trash = copyStr.slice( 0, m );
-			copyStr = copyStr.replace(trash, "");
-				
-
-			//	get rid of everything after the ';' && insert a line break
-			trash = copyStr.slice( copyStr.indexOf(";")+1, copyStr.length );
-			copyStr = copyStr.replace(trash, "");
-
-			// $log.log( 'Our string is : ' );
-			// $log.log( copyStr );
-
-			// $log.log( 'trashing: ' );
-			// $log.log( trash );
-
-
-			// $log.log( 'indexOf(margin:)' + m + 7 );
-			// $log.log( '; is @ ' + str.indexOf(";") )
-
-
-
-			
-
-			//	now the only spaces are between each of your values
-			//	we need to count how many values were added
-
-				//	if 1:
-					// apply that amount to each margin level
-				//	if 2:
-					//	apply the first amount to the top and bottom and the second to left and r
-				//	if 3:
-					//	apply the 1st to mtop, 2nd to mleft and mright, 3rd to mbottom;
-				//	if 4:
-					//	apply around to each.
-
-
-		};
-		//	If there is an instance of 'padding:'
-		if ( p >= 0 ){
-			
-
-			//	If there happens to be more than one instance of padding, we only want the values of the last listed one
-			if( appearsHowManyTimes( 'padding:', str ) > 1){
-
-				// lastIndexOf()
-
-			};
-
-		};
-
-		//	break val up into 4 separate items
-
-
-
-		return str;
-
-	};
-*/
 
 
 	function initTextareas( ) {
